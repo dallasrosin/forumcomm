d47d1da8e33eec42e049d2ed0c1ce950003ffbd5
dallasrosin <70175632+dallasrosin@users.noreply.github.com>
2020-08-26 19:42:47 -0500

    Update jQuery_widget.js

--- jQuery_widget.js
+++ jQuery_widget.js
@@ -1,10 +1,6 @@
-//Code reference: https://www.tutorialrepublic.com/faq/how-to-load-local-json-file-using-jquery.php#:~:text=Answer%3A%20Use%20the%20jQuery%20%24.,using%20a%20GET%20HTTP%20request.
 
-$(document).ready(function(){
         $.getJSON("worldnews.json", function(data){
-            console.log(data.name); // Prints: Harry
-            console.log(data.age); // Prints: 14
+            console.log(data);
         }).fail(function(){
             console.log("An error has occurred.");
         });
-    });
